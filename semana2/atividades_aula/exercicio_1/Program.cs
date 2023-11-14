cc590eae9bcb57a2d0443e292197b65056ee174e
Welvis <welvisconta@gmail.com>
2023-11-14 11:38:39 -0300

    Atividades realizadas na semana 2

---
+++ semana2/atividades_aula/exercicio_1/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] colecao = {"Item1", "Ite21", "Item3", "Item4"};
+
+foreach (string item in colecao)
+{   
+    // item = item + " Alterado";
+    colecao[0] = "item alterado";
+    Console.WriteLine(item);
+}
+
+
+Console.WriteLine("Digite seu nome: ");
+string? nome = Console.ReadLine();
+Console.WriteLine("Seu nome é: " + nome);
---
+++ semana2/atividades_aula/exercicio_2/Program.cs
@@ -0,0 +1,42 @@
+﻿#region exercicio_1
+for(int i=0;i<=30;i++){
+    if(i%3 == 0){
+        Console.WriteLine(i);
+    }else if(i%4 == 0){
+        Console.WriteLine(i);
+    }
+}
+#endregion
+
+#region exercicio_2
+int n;
+int x=1, y=0, z=0;
+n = 10;
+
+for (int i = 0; i < n; i++)
+{
+    z = x + y;
+    if(z > 100){
+        break;
+    }
+    Console.WriteLine(z);
+    y = x;
+    x = z;
+}
+Console.ReadLine();
+
+#endregion
+
+#region exercicio_3
+int n = 7;
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(i * j + " ");
+    }
+    Console.WriteLine();
+}
+
+#endregion
