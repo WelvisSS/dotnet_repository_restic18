ae2a4ecfb1fe329b5ce65ef0b64b116e306b437a
Welvis <welvisconta@gmail.com>
2023-11-16 09:18:12 -0300

    atividade pratica 1 e 2 da semana 1

---
+++ semana1/lista1/atividade2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Aluno: Welvis Silva Souza
+
+// Os tipos de dados numéricos inteiros disponíveis no .NET são:
+// sbyte, byte, short, ushort, int, uint, long, ulong
+
+// Exemplos de uso para cada um deles:
+
+// sbyte: armazena valores inteiros de -128 a 127
+sbyte sbyteExample = 10;
+
+// byte: armazena valores inteiros de 0 a 255
+byte byteExample = 20;
+
+// short: armazena valores inteiros de -32,768 a 32,767
+short shortExample = 30;
+
+// ushort: armazena valores inteiros de 0 a 65,535
+ushort ushortExample = 40;
+
+// int: armazena valores inteiros de -2,147,483,648 a 2,147,483,647
+int intExample = 50;
+
+// uint: armazena valores inteiros de 0 a 4,294,967,295
+uint uintExample = 60;
+
+// long: armazena valores inteiros de -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807
+long longExample = 70;
+
+// ulong: armazena valores inteiros de 0 a 18,446,744,073,709,551,615
+ulong ulongExample = 80;
