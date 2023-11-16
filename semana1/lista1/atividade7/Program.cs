c39f913cdbe3e37174a14e1c5fb16b6eac4e5700
Welvis <welvisconta@gmail.com>
2023-11-16 09:51:01 -0300

    atividade 7 pratica 1

---
+++ semana1/lista1/atividade7/Program.cs
@@ -0,0 +1,11 @@
+﻿bool condicao1 = true;
+bool condicao2 = false;
+
+if (condicao1 && condicao2)
+{
+    Console.WriteLine("Ambas as condições são verdadeiras");
+}
+else
+{
+    Console.WriteLine("Pelo menos uma das condições é falsa");
+}
