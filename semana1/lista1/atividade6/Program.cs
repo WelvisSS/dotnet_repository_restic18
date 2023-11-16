45d19cd83ca7fe9e722f23f6b5c12b65c06462ae
Welvis <welvisconta@gmail.com>
2023-11-16 09:48:45 -0300

    atividade 6 pratica 1

---
+++ semana1/lista1/atividade6/Program.cs
@@ -0,0 +1,11 @@
+﻿string str1 = "Hello";
+string str2 = "World";
+
+if (str1 == str2)
+{
+    Console.WriteLine("As duas strings são iguais");
+}
+else
+{
+    Console.WriteLine("As duas strings são diferentes");
+}
