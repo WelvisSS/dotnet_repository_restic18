1da8212d53149b74b2571bd0dce241d80225bb8d
Welvis <welvisconta@gmail.com>
2023-11-16 09:23:44 -0300

    atividade 3 pratica 1

---
+++ semana1/lista1/atividade3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Aluno: Welvis Silva Souza
+
+double doubleValue = 3.14;
+int intValue1 = Convert.ToInt32(doubleValue);
+int intValue2 = (int)doubleValue;
+
+Console.WriteLine("Converted value using Convert.ToInt32(): " + intValue1);
+Console.WriteLine("Converted value using casting operator: " + intValue2);
