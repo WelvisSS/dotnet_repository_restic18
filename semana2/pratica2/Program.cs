b115b08ec259dba19d9443c3f749b0ddc3b0cc8f
Welvis <welvisconta@gmail.com>
2023-11-19 16:15:55 -0300

    estrutura inicial da lista de tarefas

---
+++ semana2/pratica2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ToDoList
+{
+
+    static void Main()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("1. Criar tarefa");
+            Console.WriteLine("2. Listar todas as tarefas");
+            Console.WriteLine("3. Marcar tarefa como concluída");
+            Console.WriteLine("4. Listar tarefas pendentes");
+            Console.WriteLine("5. Listar tarefas concluídas");
+            Console.WriteLine("6. Excluir tarefa");
+            Console.WriteLine("7. Pesquisar tarefas por palavra-chave");
+            Console.WriteLine("8. Mostrar estatísticas");
+            Console.WriteLine("9. Sair");
+            Console.WriteLine("Escolha uma opção: ");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Escolha novamente.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Escolha novamente.");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+}
