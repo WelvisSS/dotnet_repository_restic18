ec72c809780007cc29108b3074fff8ff538b0c38
Welvis <welvisconta@gmail.com>
2023-12-14 14:43:05 -0300

    limpando main

--- semana1/lista1/atividade7/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿bool condition1 = true;
-bool condition2 = false;
-
-if (condition1 && condition2){
-    Console.WriteLine("Both conditions are true");
-}else{
-    Console.WriteLine("At least one of the conditions is false");
-}
