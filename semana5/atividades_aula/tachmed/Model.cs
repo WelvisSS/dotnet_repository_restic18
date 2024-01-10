94afd934cf5657d3915d3446eb34a4601a97f963
Welvis <welvisconta@gmail.com>
2024-01-10 11:37:48 -0300

    atvividades semana 5

---
+++ semana5/atividades_aula/tachmed/Model.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Techmed.Model;
+
+
+public class TechmedContext : DbContext
+{
+    public DbSet<Medico> Medicos { get; set; }
+
+    public DbSet<Paciente> Pacientes { get; set; }
+
+    public TechmedContext()
+    {
+        Database.EnsureCreated();
+        Database.EnsureDeleted();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        var stringConexao = "server=localhost;user=root;password=2496;database=techmed";
+        var serverVersion = ServerVersion.AutoDetect(stringConexao);
+        optionsBuilder.UseMySql(stringConexao, serverVersion);
+    }
+
+}
+
+public abstract class Pessoa
+{
+    public string? nome { get; set; }
+    public string? cpf { get; set; }
+    public DateTime dataNascimento { get; set; }
+}
+
+public class Paciente : Pessoa
+{
+    public string? sexo { get; set; }
+    public string? sintomas { get; set; }
+}
+
+public class Medico : Pessoa
+{
+    public int crm { get; set; }
+}
