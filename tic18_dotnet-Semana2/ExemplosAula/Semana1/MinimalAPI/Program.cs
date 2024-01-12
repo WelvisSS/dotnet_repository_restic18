e4f435d5171cd85c6aa8dc74697faafd9a31cf58
Welvis <welvisconta@gmail.com>
2024-01-12 14:05:45 -0300

    semana 7

--- tic18_dotnet-Semana2/ExemplosAula/Semana1/MinimalAPI/Program.cs
+++
@@ -1,9 +0,0 @@
-using Semana1.MinimalAPI;
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/helder/", () => Helder.View());
-
-app.Run();
