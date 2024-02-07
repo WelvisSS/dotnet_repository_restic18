ed51de56c525e7277d94e2141a77ee5062d08b17
Welvis <welvisconta@gmail.com>
2024-02-07 09:10:30 -0300

    Delete unnecessary files and configurations

---
+++ Modulo3/TechMed.Application/Services/AuthService.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+namespace TechMed.Application;
+
+public class SimpleAuthHandle
+{
+    private readonly RequestDelegate _next;
+
+    public SimpleAuthHandle(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.ContainsKey("Authorization"))
+        {
+            context.Response.Headers.Append("WWW-Authenticate", "Basic");
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Authorization header is missing");
+            return;
+        }
+
+        string header = context.Request.Headers["Authorization"].ToString();
+        string encodedUserNamePassword = header.Substring(6);
+        string decodedUserNamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUserNamePassword));
+        string userName = decodedUserNamePassword.Split(":")[0];
+        string password = decodedUserNamePassword.Split(":")[1];
+
+        if (userName != "admin" || password != "admin")
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Invalid username or password");
+            return;
+        }
+
+        await _next(context);
+    }
+}
+
+public static class AuthMiddlewareExtensions
+{
+    public static IApplicationBuilder AuthMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<SimpleAuthHandle>();
+    }
+}
