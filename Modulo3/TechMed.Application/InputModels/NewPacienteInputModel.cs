9aff3c408e933f8e5f72c47993d0577c76696362
Welvis <welvisconta@gmail.com>
2024-02-09 16:18:33 -0300

    aula

--- Modulo3/TechMed.Application/InputModels/NewPacienteInputModel.cs
+++ Modulo3/TechMed.Application/InputModels/NewPacienteInputModel.cs
@@ -3,6 +3,6 @@ namespace TechMed.Application.InputModels
     public class NewPacienteInputModel
     {
         public string Nome { get; set; }
-        // public string Cpf { get; set; }
+        public string Cpf { get; set; }
     }
 }
--- Modulo3/TechMed.Application/Validators/NewPacienteValidator.cs
+++ Modulo3/TechMed.Application/Validators/NewPacienteValidator.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Text.RegularExpressions;
 using FluentValidation;
 using TechMed.Application.InputModels;
 
@@ -10,5 +11,22 @@ public class NewPacienteValidator : AbstractValidator<NewPacienteInputModel>
     {
         RuleFor(x => x.Nome)
           .NotEmpty().WithMessage("O nome do paciente é obrigatório").MinimumLength(3).WithMessage("O nome do paciente deve ter no mínimo 3 caracteres");
+
+        RuleFor(x => x.Cpf)
+          .NotEmpty().WithMessage("O CPF do paciente é obrigatório").
+          MinimumLength(11).
+          WithMessage("O CPF do paciente deve ter no mínimo 11 caracteres").
+          Must(validateCpf).WithMessage("O cpf deve estar no formato 000.000.000-00");
+
     }
+
+    public static bool validateCpf(string cpf)
+    {
+        string cpfPattern = @"^\d{3}\.\d{3}\.\d{3}-\d{2}$";
+
+        Regex regex = new Regex(cpfPattern);
+
+        return regex.IsMatch(cpf);
+    }
+
 }
